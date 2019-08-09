f1670b5772a314e44bf839e313be483439cde18c
svartalf <self@svartalf.info>
2019-08-09 16:53:12 +0300

    Replacing threadpool with futures::executor for runtime-polyfill

--- heim-runtime/src/shims/sync/pool.rs
+++ heim-runtime/src/shims/sync/pool.rs
@@ -1,9 +1,10 @@
 use std::fmt::Debug;
 use std::sync::Mutex;
 
+use futures::executor::ThreadPool;
 use futures::channel::oneshot;
-use futures::future::Future;
-use threadpool::ThreadPool;
+use futures::future::{self, Future};
+use futures::task::Spawn;
 
 use heim_common::prelude::*;
 
@@ -16,7 +17,12 @@ pub struct FuturePool(Mutex<ThreadPool>);
 
 impl FuturePool {
     pub fn new() -> FuturePool {
-        FuturePool(Mutex::new(ThreadPool::default()))
+        let inner = ThreadPool::builder()
+            .name_prefix("heim-")
+            .create()
+            .expect("Misconfigured thread pool");
+
+        FuturePool(Mutex::new(inner))
     }
 
     pub fn spawn<F, T>(&self, f: F) -> impl Future<Output = T>
@@ -24,12 +30,18 @@ impl FuturePool {
         F: FnOnce() -> T + Send + 'static,
         T: Send + Debug + 'static,
     {
-        let pool = self.0.lock().expect("Thread pool mutex is poisoned");
 
         let (tx, rx) = oneshot::channel();
-        pool.execute(move || {
-            let _ = tx.send(f());
-        });
+        let fut: future::FutureObj<()> = future::lazy(|_| {
+                let _ = tx.send(f());
+            })
+            .boxed()
+            .into();
+
+        {
+            let mut pool = self.0.lock().expect("Futures pool mutex is poisoned");
+            let _ = pool.spawn_obj(fut);
+        }
 
         rx.map(|res| res.expect("Runtime future was canceled"))
     }
